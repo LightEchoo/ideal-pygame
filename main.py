e7ac666a548694049c934bda67d81e5d5697d56b
TaXueGeXing <comet_rack@yeah.net>
2023-12-23 16:53:41 +0000

    main changes

--- main.py
+++ main.py
@@ -9,32 +9,33 @@ class main:
         self.Window = pygame.init()
         self.Screen = pygame.display.set_mode((800,600))
         pygame.display.set_caption("Start It!")
-        self.Icon = pygame.image.load('floor_block.png')
+        self.Icon = pygame.image.load('images/floor_block.png')
         pygame.display.set_icon(self.Icon)
         self.fontFreesansbold = pygame.font.Font("freesansbold.ttf", 32)
+        self.Clock = 120
+        pygame.time.Clock.tick(self.Clock) #设置帧数
 
     def get_data():
         Map = map.map()
-        # Player = player.test()
-        # return Map, Player
-        return Map
+        Player = player()
+        return Map, Player
 
     def run_it(self):
-        # Map, Player = main.getData()
-        Map =  main.getData()
+        Map, Player = main.get_data()
+        # Map =  main.getData()
         Running = True
         while Running == True:
             for Event in pygame.event.get():
                 if Event.type == pygame.QUIT:
                     Running = False  #判定关闭游戏
             # self.Window.blit(Player.image, (tuple(player.Location))) #渲染主角
-            # Player.Moving() #调用移动函数
+                Player.operator() #调用移动函数
             Map.show_block_floor(self.Screen) #生成地图
-
+            Player.update()
             pygame.display.update()
 
     def event():
         pass
 
-# Run = main()
-# Run.run_it()
+Run = main()
+Run.run_it()
